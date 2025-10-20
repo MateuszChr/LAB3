53ce2a73bee7da2acd6afe089b98a32f796c5b9a
Mateusz <chryscienko24141@stud.akademiabialska.pl>
2025-10-20 19:27:08 +0200

    test322

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace LAB03
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            //testokokuygsaduagshdgasd723921
+            //test2115
         }
     }
 }
